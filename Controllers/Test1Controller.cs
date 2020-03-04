873465f1da3acb50633564c6291585cf2ef144fb
sai wanza <q020385791@gmail.com>
2020-03-04 17:23:30 +0800

    加入專案檔案。

---
+++ Controllers/Test1Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testMVC.ViewModels;
+
+namespace testMVC.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CheckIP()
+        {
+            string ip = Request.UserHostAddress;
+            CheckIPViewModel model = new CheckIPViewModel();
+            model.IP = ip;
+            return PartialView("CheckIP", model);
+        }
+
+        public ActionResult CheckTime()
+        {
+            string Time = DateTime.Now.ToString();
+            CheckTimeViewModel model = new CheckTimeViewModel();
+            model.Time = Time;
+            return PartialView("CheckTime", model);
+        }
+    }
+}
---
+++ ViewModels/CheckTimeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testMVC.ViewModels
+{
+    public class CheckTimeViewModel
+    {
+        public string Time { get; set; }
+    }
+}
