6b6ac19ea9184cfb66a1852f02279e8472441167
sai wanza <q020385791@gmail.com>
2020-03-09 16:55:14 +0800

    新增BundleConfig註解

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -6,8 +6,15 @@ namespace testMVC
     public class BundleConfig
     {
         // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+
+        //很多個js和css檔案一次打包在Layout載入
+
+        //Layout載入方式
+        //@Styles.Render("~/Content/css")
+        //@Styles.Render("~/Content/js")
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //註冊虛擬打包路徑， 所有相關檔案加入
             bundles.Add(new ScriptBundle("~/Content/css").Include(
                         "~/Content/bootstrap.min.css"
                         ));
